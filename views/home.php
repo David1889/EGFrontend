52e15535ec737d536f998cf22e9bacaccfda768a
fflorsi <fdegiusti@frro.utn.edu.ar>
2025-03-22 12:52:04 -0300

    estructura carpetas + navbar + home sin terminar

---
+++ views/home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Veterinaria San Antón</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./assets/css/home.css" rel="stylesheet"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet"></head>
+    <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+
+<body>
+    <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./assets/img/home-dog-cat.jpeg" class="d-block w-100" alt="Cuidado de Mascotas">
+                <div class="carousel-overlay"></div>
+                <div class="carousel-caption d-none d-md-block">
+                    <h1 class="title">Bienvenido a la segunda casa de tu mascota</h1>
+                    <p class="sub-title">San Anton se creó con una misión: garantizar que cada mascota reciba el amor, la atención y el cuidado que merece. Nuestro equipo está formado por peluqueros, entrenadores y veterinarios con amplia experiencia.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="./assets/img/home-dogs-slide2.jpeg" class="d-block w-100" alt="Servicios de Peluquería">
+                <div class="carousel-overlay"></div>
+                <div class="carousel-caption d-none d-md-block">
+                    <h1 class="title">Cuidado compasivo para sus mascotas</h1>
+                    <p class="sub-title">Somos un equipo apasionado de amantes de las mascotas que cree en tratar a cada animal como si fuera nuestro. Desde servicios de salud hasta peluquería y adopción, estamos aquí para acompañar a tu mascota en cada paso del camino.</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        </button>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
